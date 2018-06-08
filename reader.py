84e86dc4e8e13bbace6736637799b7b572cd612f
tab <tab88tab@gmail.com>
2018-06-08 10:52:18 -0400

    add argv input and json output

--- reader.py
+++ reader.py
@@ -1,7 +1,20 @@
-import pickle
-import generator
-output = open('meta.data','wb')
-imgPath = "200000.jpg"
-data = generator.export(imgPath)
-pickle.dump(data,output)
-output.close
+import json
+import sys
+import generator                # for export image meta into a dict
+
+import os
+if len(sys.argv)<2:
+    print("no image resource dir!")
+else:
+    output = open('meta.data','w')
+    metalist = []
+    for filename in os.listdir(sys.argv[1]):
+        filepath = os.path.join(sys.argv[1],filename)
+        if os.path.isfile(filepath):
+            print (filepath)
+            subdata = generator.export(filepath)
+            metalist.append(subdata)
+    json.dump(metalist,output)
+    output.close
+
+
